4b0ed2263f529288a4314919347a8e5de224e94f
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-06-04 12:05:03 +0530

    Update

--- schema.js
+++ schema.js
@@ -2,12 +2,12 @@ const Joi = require('joi');
 
 const listingSchema = Joi.object({
     listing: Joi.object({
-        title: Joi.string().required,
-        description: Joi.string().required,
-        price: Joi.number().min(0).required,
-        image: Joi.string().allow("", null).required,
-        location: Joi.string().required,
-        country: Joi.string().required,
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        price: Joi.number().min(0).required(),
+        image: Joi.string().allow("", null).required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
     }).required()
 });
 
