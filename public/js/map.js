7785f9f0bb7959b4201f1a1294135e9b6616aad1
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-06-18 14:41:41 +0530

    Update the map feature

--- public/js/map.js
+++ public/js/map.js
@@ -10,7 +10,11 @@
         zoom: 9 // starting zoom
     });
     const marker = new mapboxgl.Marker({color:"red"})
-    .setLngLat(listing.geometry.coordinates) //Listing.geometry.coordinates
-    .setPopup( new mapboxgl.Popup({ offset: 25 }).setText(
-        'Construction on the Washington Monument began in 1848.'))
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `<h5>${listing.title}</h5><h6>${listing.location}</h6><p>Exact location will be provided after booking</p>`
+            
+        )
+    )
     .addTo(map);
