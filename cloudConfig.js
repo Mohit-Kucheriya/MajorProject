9345691fd7c109faf1caf9e720b1c404871c0574
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-06-19 18:37:38 +0530

    Changes

--- cloudConfig.js
+++ cloudConfig.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name:dcooeoplr,
-    api_key:628111615431858,
-    api_secret:dPGx1x7NHbX00Wt9XG8DjsnMPCU
+    cloud_name:"dcooeoplr",
+    api_key:"628111615431858",
+    api_secret:"dPGx1x7NHbX00Wt9XG8DjsnMPCU"
 })
 
 const storage = new CloudinaryStorage({
